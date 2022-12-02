3aabdee6d1616018b3bd4158fd739bc9c8e5c484
emil <emil.kantaneva@gmail.com>
2022-12-02 23:49:03 +0200

    added borders to project photos

--- components/resume/WorkExp.js
+++ components/resume/WorkExp.js
@@ -12,7 +12,7 @@ const data = [
       "Helped to setup school lab for networking and security courses.",
     started: "17.10.2022",
     ended: "4.11.2022",
-    id: 3,
+    id: 1,
   },
   {
     title: "IT Support person",
@@ -30,7 +30,7 @@ const data = [
       "Helped staff and students with IT related issues, such as setting up their computers, installing software, troubleshooting, and other IT related issues. Customer service, communication and general IT skills were required to succeed in this position.",
     started: "3.5.2021",
     ended: "31.8.2021",
-    id: 1,
+    id: 3,
   },
   {
     title: "Accountant",
--- components/works/ProjectRender.js
+++ components/works/ProjectRender.js
@@ -8,21 +8,21 @@ const ProjectRender = ( {Project} ) => {
 
     return (
         <div className="">
-        <div className="flex flex-col">
+        <div className="flex flex-col text-center">
           {Project.map((project) => (
             <div className="flex flex-col my-10" key={project.id}>
-              <h1 className="bg-dark-grey md:text-4xl text-2xl p-2 text-beige-g text-center">
+              <h1 className="bg-dark-grey md:text-4xl text-2xl p-2 text-beige-g">
                 {project.title}
               </h1>
-              <div className="text-center">
+              <div className="">
               <div className="grid grid-cols-2">
                     <button
-                      className="bg-dark-grey md:text-2xl text-s p-2 text-beige-g text-center hover:text-white my-2"
+                      className="bg-dark-grey md:text-2xl text-s p-2 text-beige-g hover:text-white my-2"
                       onClick={() => setShowTags(showTags ? false : true)}
                     >
                       {showTags ? "Hide Tags" : "Show Tags"}
                     </button>
-                    <button className="bg-dark-grey md:text-2xl text-s p-2 text-beige-g text-center hover:text-white my-2">
+                    <button className="bg-dark-grey md:text-2xl text-s p-2 text-beige-g hover:text-white my-2">
                     <a
                       href={project.source}
                       target="_blank"
@@ -47,9 +47,15 @@ const ProjectRender = ( {Project} ) => {
                 )}
               </div>
  
-              <p className="text-l">{project.description}</p>
-    
-              <Image src={project.image} alt="test" />
+              <p className="text-l text-left">{project.description}</p>
+                <div className="grid border-2 border-dark-grey">
+              <Image src={project.image} alt="test" 
+                width={project.width}
+                height={project.height}
+                className=""
+                />
+                </div>
+      
     
             </div>
           ))}
